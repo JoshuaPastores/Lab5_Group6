0070cfb42e826fe733367e7f8e28365799a02448
AdrianeGutierrez <adriane.gutierrez.iics@ust.edu.ph>
2020-10-01 19:50:49 +0800

    added Personal Info

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ const server = http.createServer((req, res) => {
     if (req.url === "/") {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write("<html><body><h1>Home Page</h1></body></html>");
-        res.write("<a href='/AbalosPage'>Go to Abalos PAGE</a>")
+        res.write("<a href='/GutierrezPage'>Go to Gutierrez PAGE</a>")
         res.end();
         
-}else if (req.url === "/AbalosPage"){
+}else if (req.url === "/GutierrezPage"){
             res.writeHead(200, {"Content-Type":"text/html"});
 
-                res.write("<html><body style='background-color:grey;'><center><h1>NAME: Abalos Timothy James</h1></center></body></html>");
+                res.write("<html><body style='background-color:yellow;'><center><h1>NAME: Gutierrez, Adriane</h1></center></body></html>");
                                                                                       
-               res.write("<html><body><center><h1>EMAIL: timothyabalos02@gmail.com</h1></center></body></html>");
+               res.write("<html><body><center><h1>EMAIL: adriane.gutierrez.iics@ust.edu.ph</h1></center></body></html>");
                                                                                       
-               res.write("<html><body><center><h1>AGE: 21</h1></center></body></html>");
+               res.write("<html><body><center><h1>AGE: 22</h1></center></body></html>");
                                                                                                               
-               res.write("<html><body><center><h1>HOBBIES: Collecting Sneakers</h1></center></body></html>");
+               res.write("<html><body><center><h1>HOBBIES: Playing the Ukulele</h1></center></body></html>");
                                                                                       
                res.write("<html><body><center><h1>SCHOOL: University of Santo Tomas</h1></center></body></html>");
                                                                                                                     
